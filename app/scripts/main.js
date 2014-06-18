8035da8fe40d0a1b61acb13d603c335a08a4cd85
Heather Amler <heatheramler@yahoo.com>
2014-06-17 23:32:11 -0400

    Wiped out old JS and added Views with additional CSS

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,38 +1,65 @@
-Parse.initialize("9FROBMPXAABxeBZaG0K7j8wlnPtaM5sFYJLeXE9B", "blDjPSaVIg7SK7jfGk9v1tMDQXXiheID6tgwFgRH");
-console.log('WTF do you think you\'re trying to do here!?!');
+Parse.initialize('9FROBMPXAABxeBZaG0K7j8wlnPtaM5sFYJLeXE9B', 'blDjPSaVIg7SK7jfGk9v1tMDQXXiheID6tgwFgRH');
 
 var Photo = Parse.Object.extend({
 	className: 'Photo'
 });
-/*
-var PhotoCollection = Parse.Collection.extend({
-	model: Photo;
-})*/
 
-/*var photo = new Photo();*/
 
-/*
-var inputValue;
+//////////////////////////////
+// Thumbnail View
+//////////////////////////////
 
-$('.submit-photo').click(function() {
-	inputValue = $('.photo-uploader').val();
-	//console.log(inputValue);
-	//console.log(typeof inputValue);
-	photo.set('url', inputValue);
+var PhotoView = Parse.View.extend({
+	template: _.template($('.thumbnails-display-template').text()),
 
-	photo.save(null, {
-	  success: function(photo) {
-	    // Execute any logic that should take place after the object is saved.
-	    alert('New object created with objectId: ' + photo.id);
- 	  },
-	  error: function(photo, error) {
-	    // Execute any logic that should take place if the save fails.
-	    // error is a Parse.Error with an error code and description.
-	    alert('Failed to create new object, with error code: ' + error.description);
-	  }
-	});
+	className: 'image-holder',
+
+	events: {
+		'click .submit-photo' : 'uploadPhoto'
+	},
+
+	initialize: function() {
+		$('.photo-container').append(this.el);
+		this.render();
+	},
+
+	render: function(){
+		var renderedTemplate = this.template(this.model.attributes)
+    // empty div // html render the variable above
+		this.$el.html(renderedTemplate);
+	}
 });
-*/
+
+//////////////////////////////
+// Detail View
+//////////////////////////////
+
+var DetailView = Parse.View.extend({
+	template: _.template($('.detail-display-template').text()),
+	className: 'main-image-holder', 
+		
+	initialize: function() {
+		$('.photo-container').append(this.el);
+		this.render();
+	},
+
+	render: function(){
+		var renderedTemplate = this.template(this.model.attributes)
+    // empty div // html render the variable above
+		this.$el.html(renderedTemplate);
+	},
+})
+
+
+//////////////////////////////
+// Instances
+//////////////////////////////
+
+var photoViewer = new PhotoView();
+
+var photo = new Photo();
+
+var inputValue;
 
 $('.submit-photo').click(function() {
 	var fileUploadControl = $('.profilePhotoFileUpload')[0];
@@ -56,95 +83,6 @@ $('.submit-photo').click(function() {
 	console.log(photo);
 	console.log(parseFile);
 	console.log(captionValue);
-		debugger
 	photo.save();
 
 });
-
-
-function retrieve(){
-    var register = Parse.Object.extend("register");
-    var query = new Parse.Query(register);
-    var self = this;
-    var email = this.$("#retrieve-email").val();
-    query.equalTo("email", email);
-    query.find({
-    success: function(results) {
-    alert("Successfully retrieved " + results.length + ".");
-        imageURLs = [];  
-    for (var i = 0; i < results.length; i++) {
-        var object = results[i];
-        var image=imageURLs.push(object.get('image'));
-        //var image =results[i].get("image").url;
-        if(imageURLs.length > 0){
-          $('#color').attr('src', imageURLs[0]);
-        }
-       }
-    },
-  error: function(error) {
-    alert("Error: " + error.code + " " + error.message);
-  }
-	});
-}
-
-
-
-// var PhotoView = Parse.View.extend({
-
-// 	template: _.template($('.image-display-template').text()),
-// 	//newTemplate: _.template($('.image-add-detail-template').text()),
-
-// 	events: {
-// 		'click .submit-photo'	: 'editImage'
-// 	},
-
-// 	initialize: function() {
-// 		// MAYBE???? telling this view to reredner any changes to the model
-// 		/*this.listenTo(photo, 'add', function(image) {
-// 			new PhotoView({model: image})
-// 		})*/
-
-// 		$('.photo-container').prepend(this.el);
-// 		this.render();
-// 	},
-
-// 	render: function() {
-// 		/*var renderedTemplate = this.template(this.model)
-// 		//console.log(this.model.attributes)
-// 		this.$el.html(renderedTemplate);
-// 		// Idea with render is that you should generally return this.
-// 		// The reason is that you can keep chaining stuf onto render
-// 		return this;*/
-
-// 		$(this.el).html(this.template(this.model))
-// 	},
-// });
-
-
-// var images = new PhotoView();
-
-
-/*initialize: function () {
-		photo.get().done(function() {
-			photo.each(function (image) {
-				new ThumbnailView({model: image});
-			});
-		});
-	},*/
-
-/*var GameScore = Parse.Object.extend("GameScore");
-var query = new Parse.Query(photo);
-query.find({
-  success: function(results) {
-    alert("Successfully retrieved " + results.length + ".");
-    // Do something with the returned Parse.Object values
-    for (var i = 0; i < results.length; i++) { 
-      var object = results[i];
-      alert(object.id + ' - ' + object.get('description'));
-    }
-  },
-  error: function(error) {
-    alert("Error: " + error.code + " " + error.message);
-  }
-});
-*/
