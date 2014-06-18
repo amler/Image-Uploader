188860132c85b19a23a0664c5972f9ed4e9416ed
Heather Amler <heatheramler@yahoo.com>
2014-06-18 13:56:51 -0400

    Reworked code

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -9,14 +9,10 @@ var Photo = Parse.Object.extend({
 // Thumbnail View
 //////////////////////////////
 
-var PhotoView = Parse.View.extend({
+/*var PhotoView = Parse.View.extend({
 	template: _.template($('.thumbnails-display-template').text()),
 
 	className: 'image-holder',
-
-	events: {
-		'click .submit-photo' : 'uploadPhoto'
-	},
 
 	initialize: function() {
 		$('.photo-container').append(this.el);
@@ -28,7 +24,7 @@ var PhotoView = Parse.View.extend({
     // empty div // html render the variable above
 		this.$el.html(renderedTemplate);
 	}
-});
+});*/
 
 //////////////////////////////
 // Detail View
@@ -39,30 +35,54 @@ var DetailView = Parse.View.extend({
 	className: 'main-image-holder', 
 		
 	initialize: function() {
-		$('.photo-container').append(this.el);
-		this.render();
+		retrieve();
 	},
 
-	render: function(){
-		var renderedTemplate = this.template(this.model.attributes)
-    // empty div // html render the variable above
-		this.$el.html(renderedTemplate);
-	},
+	// render: function(){
+	// 	var renderedTemplate = this.template(this.model.attributes)
+ //    // empty div // html render the variable above
+	// 	this.$el.html(renderedTemplate);
+	// },
 })
+
+function retrieve() {
+	var Photo = Parse.Object.extend("Photo");
+	console.log(Photo);
+	debugger
+	var query = new Parse.Query(Photo);
+	query.descending('createdAt');
+	query.limit(25);
+	query.find({
+		success: function (results) {
+		alert("Successfully retrieved " + results.length + ".");
+			var templateFunction =  _.template($('.image-display-template').text());
+			_.each(results, function (photoModel){
+				console.log('photoModel is', photoModel)
+				$('.photo-container').append(templateFunction(photoModel.attributes))
+			})
+		},
+		error: function(error) {
+		alert("Error: " + error.code + " " + error.message);
+		}
+	});
+}
 
 
 //////////////////////////////
 // Instances
 //////////////////////////////
 
-var photoViewer = new PhotoView();
+// var thumbnailView = new PhotoView();
+// var mainImageView = new DetailView();
 
 var photo = new Photo();
 
 var inputValue;
 
 $('.submit-photo').click(function() {
+
 	var fileUploadControl = $('.profilePhotoFileUpload')[0];
+	console.log(fileUploadControl.files.length);
 	if (fileUploadControl.files.length > 0) {
 		var file = fileUploadControl.files[0];
 		var name = 'photo.jpg';
@@ -76,13 +96,10 @@ $('.submit-photo').click(function() {
 	// The file either could not be read, or could not be saved to Parse.
 	    alert('Failed to create new object, with error code: ' + error.description);
 	});
-
 	photo = new Parse.Object('Photo');
 	photo.set('description', captionValue);
 	photo.set('photo', parseFile);
 	console.log(photo);
 	console.log(parseFile);
-	console.log(captionValue);
 	photo.save();
-
 });
