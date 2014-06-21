d3d21c64db929f266b2fb4908d6a86d45e497c61
Heather Amler <heatheramler@yahoo.com>
2014-06-21 09:38:25 -0400

    Added Fabric Text button

--- app/scripts/render-fabric.js
+++ app/scripts/render-fabric.js
@@ -7,7 +7,7 @@ var canvas = new fabric.Canvas('canvas');
 //////////////////////////
 // simple rectangle
 //////////////////////////
-var rect = new fabric.Rect({
+/*var rect = new fabric.Rect({
       top : 100,
       left : 100,
       width : 60,
@@ -17,7 +17,7 @@ var rect = new fabric.Rect({
 
 canvas.add(rect);
 
-
+*/
 //////////////////////////
 // Image from a url
 //////////////////////////
@@ -26,40 +26,84 @@ canvas.add(rect);
 // Button add to canvas
 //////////////////////////
 $('.add-rectangle').click(function() {
-fabric.Image.fromURL('http://th04.deviantart.net/fs31/PRE/i/2008/200/c/2/ScrappinCop_Big_Ole_Googly_Eye_by_debh945.png', function(image) {
-      image.scale(0.1);
-      canvas.add(image);
-});
-
+      fabric.Image.fromURL('http://th04.deviantart.net/fs31/PRE/i/2008/200/c/2/ScrappinCop_Big_Ole_Googly_Eye_by_debh945.png', function(image) {
+            image.scale(0.1);
+            canvas.add(image);
+      });
 })
 
+// Initial Googly Eye loader
+/*
 fabric.Image.fromURL('http://th04.deviantart.net/fs31/PRE/i/2008/200/c/2/ScrappinCop_Big_Ole_Googly_Eye_by_debh945.png', function(image) {
       image.scale(0.1);
       canvas.add(image);
-});
+});*/
 
 //////////////////////////
 // Path and path group
 //////////////////////////
 
-var path = new fabric.Path('M 0 0 L 200 100 L 170 200 z');
+// Simple triangle path - You can create a path by hand BUT use the built in Fabric parser
+/*var path = new fabric.Path('M 0 0 L 200 100 L 170 200 z');
 path.set({ left: 120, top: 120 });
-canvas.add(path);
-
+canvas.add(path);*/
 
 //////////////////////////
 // Image filter
 //////////////////////////
 
 fabric.Image.fromURL('http://theironyard.com/images/education/academy/front-end-2/graduates/joe-tamburro.jpg', function(img) {
-  // img.filters.push(new fabric.Image.filters.Sepia());
-  // img.applyFilters(canvas.renderAll.bind(canvas));
-      img.scale(0.5);
-  canvas.add(img);
+      canvas.add(img);
 });
 
 fabric.Image.fromURL('images/pug.jpg', function(img) {
-  img.filters.push(new fabric.Image.filters.Sepia());
-  img.applyFilters(canvas.renderAll.bind(canvas));
-  canvas.add(img);
+      img.filters.push(new fabric.Image.filters.Sepia());
+      img.applyFilters(canvas.renderAll.bind(canvas));
+      canvas.add(img);
 });
+
+fabric.Image.fromURL('http://1.media.dorkly.cvcdn.com/57/82/014c67406a541837b841a9b7ac6cfed9-corgi-link-cosplay.jpg', function(img) {
+      img.scale(0.5);
+
+      //img.filters.push(new fabric.Image.filters.Sepia());
+      
+      // applyFilters produces  Failed to execute 'getImageData' on 'CanvasRenderingContext2D': The canvas has been tainted by cross-origin data.
+      // img.applyFilters(canvas.renderAll.bind(canvas));
+      canvas.add(img);
+});
+
+//////////////////////////
+// Gradient filter
+//////////////////////////
+
+var circle = new fabric.Circle({
+  left: 300,
+  top: 300,
+  radius: 80
+});
+
+circle.setGradient('fill', {
+  x1: 0,
+  y1: 0,
+  x2: 0,
+  y2: circle.height / 2,
+  colorStops: {
+    0: "F08",
+    1: "blue"
+  }
+});
+
+canvas.add(circle);
+
+//////////////////////////
+// Fabric Text Button test
+//////////////////////////
+
+$('.submit-text').click(function(){
+      var fabricText = $('.text-canvas').val();
+      var canvasText = new fabric.Text(fabricText, {
+            fontFamily: 'Arial'
+      });
+      canvas.add(canvasText);
+})
+
